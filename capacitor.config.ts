ee075ff1eb8e4f31bdac1427d5412abcacef298a
Jihad Sayed <david.pales1@gmail.com>
2024-12-31 09:32:09 -0600

    starting the app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Neetechs_Fix',
+  webDir: 'www'
+};
+
+export default config;
